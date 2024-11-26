0d40fa7558d84388866cef58638161393c469404
Gabriel Freitas <gabrielluiz42@hotmail.com>
2024-11-25 23:37:40 -0300

    exibição da avaliação em tela e do cardapio

--- app.py
+++ app.py
@@ -1,14 +1,13 @@
 from modelos.restaurante import Restaurante
-
+    
 restaurante_praca = Restaurante('praça', 'Gourmet')
-restaurante_praca.receber_avaliacao('Samir', '10')
-restaurante_praca.receber_avaliacao('Rudney', '6')
-restaurante_praca.receber_avaliacao('Silvia', '8')
-
-restaurante_praca.listar_cardapio('Lasanha', 'coca-cola', 'sorvete')
+restaurante_praca.receber_avaliacao('Samir', 10)
+restaurante_praca.receber_avaliacao('Rudney', 6)
+restaurante_praca.receber_avaliacao('Silvia', 3)
 
 def main():
     Restaurante.listar_restaurantes()
+    restaurante_praca.listar_cardapio(['Lasanha'], ['Coca-Cola'], ['Sorvete'])
 
 if __name__ == '__main__':
     main()
--- modelos/restaurante.py
+++ modelos/restaurante.py
@@ -19,9 +19,9 @@ class Restaurante:
     
     @classmethod
     def listar_restaurantes(cls):
-        print(f'{'nome do restaurante'.ljust(25)} | {'Categoria'.ljust(25)} | {'Status'.ljust(25)}\n')
+        print(f'{'nome do restaurante'.ljust(25)} | {'Categoria'.ljust(25)} | {'Avaliação'.ljust(25)} | {'Status'.ljust(25)}\n')
         for restaurante in cls.restaurantes:
-            print(f'{restaurante._nome.ljust(25)} | {restaurante.categoria.ljust(25)} | {restaurante._ativo}')
+            print(f'{restaurante._nome.ljust(25)} | {restaurante.categoria.ljust(25)} | {str(restaurante.media_avaliacoes).ljust(25)} | {restaurante._ativo}\n')
             
     @property
     def ativo(self):
@@ -33,7 +33,24 @@ class Restaurante:
     def receber_avaliacao(self, cliente, nota):
         avaliacao = Avaliacao(cliente, nota)
         self._avaliacao.append(avaliacao)
+ 
+    @property  
+    def media_avaliacoes(self):
+        if not self._avaliacao:
+            return 0
+        soma_das_notas = sum(avaliacao._nota for avaliacao in self._avaliacao)
+        quantidade_de_notas = len(self._avaliacao)
+        media = round(soma_das_notas / quantidade_de_notas, 1)
+        return media
         
-    def listar_cardapio(self,pratos, bebidas, sobremesas):
-        pratos = Pratos(pratos, bebidas,sobremesas)
-        self._pratos.append(pratos)
+    def listar_cardapio(self, pratos, bebidas, sobremesas):
+        print("Cardápio:")
+        print("Pratos Principais:")
+        for prato in pratos:
+            print(f"- {prato}")
+        print("Bebidas:")
+        for bebida in bebidas:
+            print(f"- {bebida}")
+        print("Sobremesas:")
+        for sobremesa in sobremesas:
+            print(f"- {sobremesa}")    
