882a0340f21f1c1b1e9f1cdfaa9b532c0517ace4
Gabriel Freitas <gabrielluiz42@hotmail.com>
2024-11-27 01:04:43 -0300

    realizando ajustes no app.py para aparecer o restaurante e as avaliações

--- app.py
+++ app.py
@@ -1,12 +1,23 @@
 from modelos.restaurante import Restaurante
 from modelos.cardapio.bebida import Bebida
 from modelos.cardapio.prato import Prato
-    
+
 restaurante_praca = Restaurante('praça', 'Gourmet')
+restaurante_mexicano = Restaurante('Mexican Food', 'Comida mexicana')
+restaurante_japones = Restaurante('Japa', 'Japonesa')
+
+restaurante_mexicano.alternar_estado()
+restaurante_japones.receber_avaliacao('Samir', 5)
+restaurante_mexicano.receber_avaliacao('Rudney', 1)
+restaurante_praca.receber_avaliacao('Silvia', 3)
+    
 bebida_suco = Bebida('Suco de melancia',5.0,'Grande')
 prato_pao = Prato('Pao',2.0,'O melhor pão da cidade')
 
 def main():
+    Restaurante.listar_restaurantes()
+    Restaurante.media_avaliacoes
+    print('Cardapio:')
     print(bebida_suco)
     print(prato_pao)
 
