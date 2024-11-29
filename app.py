eae9d4505c0d5b4642420b870e745471d0f80593
Gabriel Freitas <gabrielluiz42@hotmail.com>
2024-11-29 00:34:20 -0300

    criando a exibição de itens do cardapio e aplicações de desconto em cada item

--- app.py
+++ app.py
@@ -3,23 +3,21 @@ from modelos.cardapio.bebida import Bebida
 from modelos.cardapio.prato import Prato
 
 restaurante_praca = Restaurante('praça', 'Gourmet')
-restaurante_mexicano = Restaurante('Mexican Food', 'Comida mexicana')
-restaurante_japones = Restaurante('Japa', 'Japonesa')
 
-restaurante_mexicano.alternar_estado()
-restaurante_japones.receber_avaliacao('Samir', 5)
-restaurante_mexicano.receber_avaliacao('Rudney', 1)
 restaurante_praca.receber_avaliacao('Silvia', 3)
     
 bebida_suco = Bebida('Suco de melancia',5.0,'Grande')
+bebida_suco.aplicar_desconto()
 prato_pao = Prato('Pao',2.0,'O melhor pão da cidade')
+prato_pao.aplicar_desconto()
+restaurante_praca.adicionar_no_cardapio(bebida_suco)
+restaurante_praca.adicionar_no_cardapio(prato_pao)
+
 
 def main():
     Restaurante.listar_restaurantes()
     Restaurante.media_avaliacoes
-    print('Cardapio:')
-    print(bebida_suco)
-    print(prato_pao)
+    restaurante_praca.exibir_cardapio
 
 if __name__ == '__main__':
     main()
